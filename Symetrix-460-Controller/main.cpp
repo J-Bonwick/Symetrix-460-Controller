67a958d9440e860ec6d93fc2d6b81f290e47cfb4
J-BONWICK <33333881+J-Bonwick@users.noreply.github.com>
2020-07-23 20:43:15 +1000

    Add Organization Domain

--- Symetrix-460-Controller/main.cpp
+++ Symetrix-460-Controller/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QCoreApplication::setOrganizationName("bonwick.dev");
+    QCoreApplication::setOrganizationDomain("bonwick.dev");
     QCoreApplication::setApplicationName("Symetrix460Controller");
     QCoreApplication::setApplicationVersion("0.1");
     MainWindow w;
